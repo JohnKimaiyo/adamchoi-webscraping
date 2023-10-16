2650058a9cdeceb391a07e6a7d9319e269d652e3
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-10-16 13:20:20 +0300

    final comit

--- adamchoi.py
+++ adamchoi.py
@@ -4,3 +4,9 @@ website="https://www.adamchoi.co.uk/overs/detailed"
 driver=webdriver.Chrome()
 driver.get(website)
 
+all_matches_button = driver.find_element_by_xpath('//label[@analytics-event="All matched"]')
+all_matches_button.click()
+
+
+
+
